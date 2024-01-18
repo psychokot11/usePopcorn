4a9bee669b3a53623184990c25028c300b931ab3
Zofia Stawarska <z.stawarska@gmail.com>
2024-01-18 22:52:36 +0100

    add cleanup function

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -20,6 +20,9 @@ function Search({ query, setQuery }) {
     }
 
     document.addEventListener("keydown", callback)
+    return () => {
+      document.addEventListener("keydown", callback)
+    }
   }, [setQuery])
 
   return (
