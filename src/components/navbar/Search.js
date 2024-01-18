784444ab7194781bf82b7166f89dcb5eb363930e
Zofia Stawarska <z.stawarska@gmail.com>
2024-01-18 22:26:08 +0100

    add focus on search bar

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const focusOnInputEl = () => {
+      inputEl.current.focus();
+    }
+
+    focusOnInputEl();
+
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.key === "Enter") {
+        focusOnInputEl();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback)
+  }, [setQuery])
+
   return (
     <>
       <input
@@ -9,6 +30,7 @@ function Search({ query, setQuery }) {
           placeholder="Search movies..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          ref={inputEl}
         />
     </>
   );
